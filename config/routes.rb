a6ac61c039384e789bec794499d44631b33562c2
jaehyunkim <jaehyunkim@gmail.com>
2011-09-27 22:52:57 +0900

    gem active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 WebApp::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   devise_for :users, :controllers => { :sessions => "user/sessions", :registrations => "user/registrations" } 
 
   resources :properties
