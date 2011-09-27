7f8a259f992d6088c79261349e288e98f1f849a2
gamtoggi <gamtoggi@thinkreals.com>
2011-09-27 15:44:26 +0900

    sessions controller override

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # protect_from_forgery
+  
+  def render_success_json(msg = nil)
+    render json: {
+      status_code: 'success',
+      message: msg || 'Success.'
+    }
+  end                           
+  
+  def render_fail_json(msg = nil)
+    render json: {
+      status_code: 'fail',
+      message: msg || 'Fail.'
+    }
+  end
+  
+  def render_require_auth_json(msg = nil)
+    render json: {
+      status_code: 'require_auth',
+      message: msg || 'Please sign in.'
+    }
+  end
 end
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,2 +1,26 @@
 class User::SessionsController < Devise::SessionsController
+  def create
+    respond_to do |format|
+      format.html { super }
+      format.json { 
+        warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#fail")
+        render_success_json('Successfully signed in.')
+      } 
+    end
+  end 
+
+  # response by json format when login fail.
+  def fail    
+    render_fail_json(flash[:alert])
+  end
+  
+  def destroy
+    respond_to do |format|
+      format.html { super }
+      format.json {
+        sign_out(current_user)  
+        render_success_json('Successfully signed out.')
+      }
+    end
+  end
 end
