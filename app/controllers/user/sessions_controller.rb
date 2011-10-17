c050ee895196f2e9ec5d7e41205ad9fcdf2ce90d
gamtoggi <gamtoggi@thinkreals.com>
2011-10-17 15:26:03 +0900

    device로 로그인 시 app_delete_at를 초기화 해준다.

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -4,6 +4,13 @@ class User::SessionsController < Devise::SessionsController
       format.html { super }
       format.json { 
         warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#fail")
+          
+        # device로 로그인 시 app_delete_at를 초기화 해준다. 
+        if (!params[:device].blank?) and (params[:device].in? %w( ios android ))
+          user = User.find_by_email(params[:user][:email])
+          user.user_setting.update_attributes app_deleted_at: nil
+        end
+        
         render_success_json('Successfully signed in.')
       } 
     end
